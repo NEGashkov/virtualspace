cba918accaf7952cbc6125fee23681a5391487b1
Nick Gashkov <nick.eugene.gashkov@gmail.com>
2017-09-19 00:48:36 +0300

    Add capabilities for adding stretchable widgets to view using grid system

--- virtualspace/utils/views/base/views.py
+++ virtualspace/utils/views/base/views.py
@@ -1,8 +1,11 @@
 # Copyright (c) 2017 Nick Gashkov
 #
 # Distributed under MIT License. See LICENSE file for details.
+import logging
 
 from PyQt5.QtWidgets import QWidget, QGridLayout
+
+log = logging.getLogger(__name__)
 
 
 class BaseView(QWidget):
@@ -40,13 +43,24 @@ class BaseView(QWidget):
     def init_layout(self):
         self.layout = QGridLayout(self)
 
-        for row, widgets_str in enumerate(self.grid):
-            for col, widget_str in enumerate(widgets_str):
-                if widget_str is None:
+        for row, widgets in enumerate(self.grid):
+            for col, widget_data in enumerate(widgets):
+                if isinstance(widget_data, tuple):
+                    widget_str, row_span, col_span = widget_data
+                elif isinstance(widget_data, str):
+                    widget_str = widget_data
+                    row_span, col_span = 1, 1
+                else:
+                    log.error('Wrong widget data for grid layout: {widget_data}'.format(widget_data=widget_data))
                     continue
 
                 widget = getattr(self, widget_str, None)
-                self.layout.addWidget(widget, row, col)
+                if widget is None:
+                    log.error('Cannot fetch widget {widget_str} for {cls}'.format(
+                        widget_str=widget_str, cls=self.__class__.__name__)
+                    )
+                else:
+                    self.layout.addWidget(widget, row, col, row_span, col_span)
 
     @property
     def stacked_widget(self):
