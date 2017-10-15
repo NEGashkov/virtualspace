064297b45add88fe05503fc0eb0954ccc3859484
Nick Gashkov <nick.eugene.gashkov@gmail.com>
2017-10-15 14:34:18 +0300

    Implement base sign up functionality

--- virtualspace/controllers/sign_in.py
+++ virtualspace/controllers/sign_in.py
@@ -9,7 +9,7 @@ from virtualspace.utils.models.query import get_instance_or_none
 class SignInScreen(Screen):
     def do_login(self, username, password):
         sa_session = get_app_sa_session()
-        query = (Account.username == username and Account.password == password)
+        query = ((Account.username == username) & (Account.password == password))
         instance = get_instance_or_none(sa_session, Account, query)
 
         if instance is None:
--- virtualspace/controllers/sign_up.py
+++ virtualspace/controllers/sign_up.py
@@ -1,5 +1,38 @@
 from kivy.uix.screenmanager import Screen
+from sqlalchemy.sql import exists
+
+from virtualspace import settings
+from virtualspace.models import Account
+from virtualspace.utils.db import get_app_sa_session
+from virtualspace.utils.text import capfirst
+from virtualspace.utils.translation import gettext as _
 
 
 class SignUpScreen(Screen):
-    pass
+    def do_sign_up(self, username, email, password, confirm_password):
+        sa_session = get_app_sa_session()
+
+        self.username = username
+        self.email = email
+        self.password = password
+        self.confirm_password = confirm_password
+
+        success = self.__check(sa_session)
+
+        if success:
+            user = Account(username=username, password=password, email=email)
+            sa_session.add(user)
+            sa_session.commit()
+            self.manager.current = settings.MENU_SCREEN_NAME
+
+    def __check(self, sa_session):
+        success = True
+
+        if self.password != self.confirm_password:
+            self.ids['confirm_password_error_label'].text = capfirst(_('passwords do not match'))
+            success = False
+        if sa_session.query(exists().where(Account.username == self.username)).scalar():
+            self.ids['username_error_label'].text = capfirst(_('username is taken'))
+            success = False
+
+        return success
